76af943c0e5dbb8f04f1338f4f2bea905a48a5f7
pavanibethireddy <pavanisreedhar123@gmail.com>
2023-04-10 16:12:11 -0700

    Initial commit

---
+++ src/main/java/com/example/Firstproj/FirstprojApplication.java
@@ -0,0 +1,23 @@
+package com.example.Firstproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class FirstprojApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context  = SpringApplication.run(FirstprojApplication.class, args);
+
+		Alien a = context.getBean(Alien.class);
+		a.show();
+
+
+
+
+
+
+	}
+
+}
